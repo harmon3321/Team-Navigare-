1b81f7a2e32ba8e2cb0e733b860d93fa0166c85c
Mason Harmon <Harrrmmdawwwggg@pal-nat186-41-246.itap.purdue.edu>
2019-02-22 13:38:38 -0500

    Our initial project setup.

---
+++ Team Navigare/Team Navigare/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Team Navigare
+//
+//  Created by Mason Harmon on 2/22/19.
+//  Copyright © 2019 Navigare. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Team Navigare/Team Navigare/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Team Navigare
+//
+//  Created by Mason Harmon on 2/22/19.
+//  Copyright © 2019 Navigare. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
